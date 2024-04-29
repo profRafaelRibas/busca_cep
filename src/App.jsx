9f2738b9cd5f5d01d76bf15d133ee6288e649a25
Rafael Ribas <133046889+profRafaelRibas@users.noreply.github.com>
2024-04-29 07:53:05 -0300

    alteração no export default no inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-function App() {
+export default function App() {
   return (
     <div className="container">
       <h1 className="titulo">Busca CEP</h1>
@@ -18,5 +18,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
